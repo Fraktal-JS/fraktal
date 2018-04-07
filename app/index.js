e4f1b68da5a8645737e4c960b7f300257e0a9619
HyperCoder <bryan.pikaardjr@outlook.com>
2018-04-07 19:44:59 -0400

    Show after ready

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 let window;
 
@@ -9,31 +9,34 @@ function create() {
         titleBarStyle: "customButtonsOnHover",
         frame: false,
         width: 954,
-        height: 516
+        height: 516,
+        show: false
     });
 
     window.maximize();
 
     window.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
         slashes: true
     }));
 
     window.setMenu(null);
     //window.webContents.openDevTools();
 
-    window.on('closed', () => { window = null; });
+    window.on("closed", () => { window = null; });
+
+    window.once("ready-to-show", () => window.show());
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
     create();
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (!window) create();
 });
